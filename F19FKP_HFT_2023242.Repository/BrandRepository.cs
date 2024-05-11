9cc584ece07807847781d1617309b99a9bb28a24
Holik Gellért <holikgellert@stud.uni-obuda.hu>
2024-05-11 17:18:36 +0200

    Repositories madecreated

---
+++ F19FKP_HFT_2023242.Repository/BrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F19FKP_HFT_2023242.Repository
+{
+    internal class BrandRepository
+    {
+    }
+}
