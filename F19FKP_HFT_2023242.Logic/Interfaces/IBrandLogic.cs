4f4f90a0a4e6e6ac9f6b4547e015caee5d1207a2
Holik Gellért <holikgellert@stud.uni-obuda.hu>
2024-05-12 13:56:25 +0200

    Updated IBrandLogic with non CRUD methods

--- F19FKP_HFT_2023242.Logic/Interfaces/IBrandLogic.cs
+++ F19FKP_HFT_2023242.Logic/Interfaces/IBrandLogic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using F19FKP_HFT_2023242.Models;
 
@@ -14,5 +15,11 @@ namespace F19FKP_HFT_2023242.Logic.Interfaces
         void Update(Brand brand);
         void Delete(int id);
         IQueryable<Brand> ReadAll();
+
+        public IQueryable<Repair> RepairsFromYear(int year);
+        public IQueryable<Car> AllCarsFromBrand(string brandName);
+        public IQueryable<Car> CarsByColor(string color);
+        public Repair MostExpensiveRepairFromBrand(string brandName);
+        public IQueryable<Car> SameWheelDriveCars(string wheelDriveName);
     }
 }
