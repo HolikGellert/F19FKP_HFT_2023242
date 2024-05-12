f6b93102bc36f893a1628f3df3e15bda675a7059
Holik Gellért <holikgellert@stud.uni-obuda.hu>
2024-05-12 17:03:56 +0200

    Endpoint Startup finished

--- F19FKP_HFT_2023242.Endpoint/Startup.cs
+++ F19FKP_HFT_2023242.Endpoint/Startup.cs
@@ -7,15 +7,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using F19FKP_HFT_2023242.Models;
+using F19FKP_HFT_2023242.Repository;
+using F19FKP_HFT_2023242.Logic;
+using F19FKP_HFT_2023242.Logic.Interfaces;
+using Castle.Core.Configuration;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Diagnostics;
+
 
 namespace F19FKP_HFT_2023242.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<BrandDbContext>();
+            services.AddTransient<IRepository<Brand>, BrandRepository>();
+            services.AddTransient<IRepository<Car>, CarRepository>();
+            services.AddTransient<IRepository<Repair>, RepairRepository>();
+
+            services.AddTransient<IBrandLogic, BrandLogic>();
+            services.AddTransient<ICarLogic, CarLogic>();
+            services.AddTransient<IRepairLogic, RepairLogic>();
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "F19FKP_HFT_2023242.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +54,25 @@ namespace F19FKP_HFT_2023242.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "F19FKP_HFT_2023242.Endpoint v1"));
             }
 
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { error = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
